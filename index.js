35213ebc7724f54b666fbc7745dc6ac7966fbc9e
zzoman <reumia@gmail.com>
2014-01-04 02:29:03 +0900

    completed

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ fs.readdir('docs/', function(err, files){
 	for( i=0; i < files.length; i++ ){
 		console.log( (i + 1) + '. ' + files[i] );
 	}
+    console.log('\n');
 
 });
 
@@ -16,8 +17,29 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function(chunk) {
-	var num = chunk - 1;
-    process.stdout.write('You selected this one : ' + files_arr[num] + '\n');
+
+    var num = chunk - 1;
+
+    fs.readFile('docs/'+files_arr[num], 'utf8', function(err, data){
+        if(data==undefined){
+
+            // 함수를 변수로 지정해서 반복 실행하는 방법 모르겠음ㅠㅠ
+            fs.readdir('docs/', function(err, files){
+
+                files_arr = files;
+                for( i=0; i < files.length; i++ ){
+                    console.log( (i + 1) + '. ' + files[i] );
+                }
+                console.log('\n');
+
+            });
+
+        }
+        else{
+            process.stdout.write('--------------------------------------------------------------\n' + marked(data) + '\n\n');
+        }
+    })
+
 });
 
 process.stdin.on('end', function() {
