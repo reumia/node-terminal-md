b028b3d600564e1ec1ebe3e9d69af640bfbe9fd5
mytory <mytory@gmail.com>
2014-01-09 15:56:55 +0900

    글 번호 잘못 입력, 엔터만 쳤을 때 등. print_list 함수화.

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
 var marked = require('marked');
 var fs = require('fs');
-
 var files_arr;
+var current_screen = 'list';
 
-fs.readdir('docs/', function(err, files){
-	
-	files_arr = files;
-	for( i=0; i < files.length; i++ ){
-		console.log( (i + 1) + '. ' + files[i] );
-	}
-    console.log('\n');
+function print_list(){
+    fs.readdir('docs/', function(err, files){
+    
+        files_arr = files;
+        for( i=0; i < files.length; i++ ){
+            console.log( (i + 1) + '. ' + files[i] );
+        }
+        current_screen = 'list';
+        console.log('\n글 번호를 입력하세요: ');
 
-});
+    });
+}
+
+print_list();
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function(chunk) {
 
+    if( /[0-9]+/.test(chunk) === false && chunk !== '\n' ){
+        process.stdout.write('글 번호를 입력하세요: \n');
+        return;
+    }
+
+    if(chunk === '\n'){
+        
+        print_list();
+        return;
+    }
+
     var num = chunk - 1;
 
     fs.readFile('docs/'+files_arr[num], 'utf8', function(err, data){
-        if(data==undefined){
-
-            // 함수를 변수로 지정해서 반복 실행하는 방법 모르겠음ㅠㅠ
-            fs.readdir('docs/', function(err, files){
-
-                files_arr = files;
-                for( i=0; i < files.length; i++ ){
-                    console.log( (i + 1) + '. ' + files[i] );
-                }
-                console.log('\n');
-
-            });
-
+        if(data === undefined){
+            console.log("번호를 잘못 입력했습니다. \n");
+            // print_list();
+        } else {
+            process.stdout.write(
+                '--------------------------------------------------------------\n' +
+                marked(data) +
+                '\n' +
+                '--------------------------------------------------------------\n' +
+                '엔터치면 목록이 나옵니다.\n'
+            );
+            current_screen = 'detail';
         }
-        else{
-            process.stdout.write('--------------------------------------------------------------\n' + marked(data) + '\n\n');
-        }
-    })
+    });
 
 });
 
